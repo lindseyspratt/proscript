7e6aa45e263191ead3e501ae114fd73958c97846
spratt <spratt@logicforsystems.com>
2018-12-05 21:57:41 -0500

    Added add_element_test predicate to simple_test.pl.

--- simple_test.pl
+++ simple_test.pl
@@ -1,3 +1,16 @@
 simple_test :-
     dom_element_attribute_value(E, id, simpletest),
     set_dom_element_inner_html(E, "Hello World").
+
+add_element_test :-
+  % create a new div element
+  create_dom_element('DIV', NewDiv),
+  % and give it some content
+  create_dom_text_node("Hi there and greetings!", NewContent),
+  % add the text node to the newly created div
+  append_dom_node_child(NewDiv, NewContent),
+
+  % add the newly created element and its content into the DOM
+  dom_element_attribute_value(CurrentDiv, id, simpletest),
+  dom_element_tag(Body, body),
+  insert_before_dom_node(Body, CurrentDiv, NewDiv).
