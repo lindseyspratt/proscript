d965384623db5ac07803eb0069fa189e35a037a0
spratt <spratt@logicforsystems.com>
2019-07-17 19:12:40 -0400

    Extended template prolog syntax decoration.
    Added syntax decoration for italic variables, blue object DSL main operators,
    and red :- operator. Extended parsing to parse variables and atoms.

--- library/template.pl
+++ library/template.pl
@@ -103,15 +103,34 @@ process(FileDir, Args, ProcessedFileList, ProcessedTail) -->
      append(Prefix, SubProcessedTail, ProcessedFileList)
     },
     process(FileDir, Args, SubProcessedTail, ProcessedTail).
-
 process(FileDir, Args, NewList, ProcessedTail) -->
-    {member(Keyword-Color, ["function"-"blue", ":-"-"blue"])},
+    {member(language-javascript, Args)
+       -> member(Keyword-Color, ["function"-"blue"])
+     ;
+     member(language-prolog, Args)
+       -> (member(Keyword-Color, [":-"-"red"])
+          ; member(Keyword, [">>", ">->", ">+>", ">*>"]),
+            Color = "blue")
+    },
     Keyword,
     !,
     {append_lists(["<span style='color:", Color, "'>", Keyword, "</span>"], AnnotatedKeyword),
      append(AnnotatedKeyword, ProcessedFileList, NewList)
     },
     process(FileDir, Args, ProcessedFileList, ProcessedTail).
+process(FileDir, Args, ProcessedFileList, ProcessedTail) -->
+    { member(language-prolog, Args)},
+    [StartCode],
+    (prolog_token(StartCode, Token)
+      -> {append(Token, SubProcessedTail, ProcessedFileList)}
+     ;
+     % Because of check above for prolog_token, this StartCode cannot be an uppercase letter in the middle of a non-var token.
+     prolog_variable(StartCode, Variable)
+           -> {append_lists(["<i>", Variable, "</i>"], Prefix),
+               append(Prefix, SubProcessedTail, ProcessedFileList)
+              }
+    ),
+    process(FileDir, Args, SubProcessedTail, ProcessedTail).
 process(FileDir, Args, NewList, ProcessedTail) -->
     [Code],
     !,
@@ -173,6 +192,7 @@ write_codes([H|T], Stream) :-
 html_escape("<", "&lt;") :- !.
 html_escape("\n", "<br>\n") :- !.
 html_escape(" ", "&nbsp;") :- !.
+html_escape("\t", "&nbsp;&nbsp;&nbsp;&nbsp;") :- !.
 html_escape(X, X).
 
 rest_of_line(LineCodes) -->
@@ -182,3 +202,77 @@ rest_of_line(LineCodes) -->
       {html_escape([Code], OutCodes),
        append(OutCodes, OtherCodes, LineCodes)},
       rest_of_line(OtherCodes).
+
+prolog_variable(StartCode, [StartCode|Variable]) -->
+    {uppercase_letter_code(StartCode)
+    ;underline_code(StartCode)
+    },
+    !,
+    alpha_numeric_underline_sequence(Variable).
+
+alpha_numeric_underline_sequence(X) -->
+    [Code],
+    ({\+ alpha_numeric_underline(Code)}
+      -> {X = []},
+         push_code(Code) % undo 'read' of Code for lookahead.
+    ;
+    {X = [Code|Y]},
+    alpha_numeric_underline_sequence(Y)
+    ).
+
+prolog_token(StartCode, [StartCode|Sequence]) -->
+    {lowercase_letter_code(StartCode)}
+      -> alpha_numeric_underline_sequence(Sequence)
+    ;
+    {quote_code(StartCode)}
+      -> quoted_sequence(StartCode, Sequence).
+
+quoted_sequence(QuoteCode, Sequence) -->
+    {backslash_code(BackslashCode)},
+    [BackslashCode],
+    !,
+    backslash_sequence(BackslashSequence),
+    {AllBackslashes = [BackslashCode|BackslashSequence],
+     list_length_parity(AllBackslashes, Parity)
+    },
+    ({Parity = odd}
+      -> [NextCode],
+         append_lists([AllBackslashes, [NextCode], Tail], Sequence)
+     ;
+     append(AllBackslashes, Tail, Sequence)
+    ),
+    quoted_sequence(QuoteCode, Tail).
+quoted_sequence(QuoteCode, Sequence) -->
+    [Code],
+    ({Code = QuoteCode}
+      -> {Sequence = [Code]}
+     ;
+     {Sequence = [Code|Tail]},
+     quoted_sequence(QuoteCode, Tail)
+    ).
+
+backslash_sequence(Sequence) -->
+    [Code],
+    ({backslash_code(Code)}
+      -> {Sequence = [Code|Tail]},
+         backslash_sequence(Tail)
+    ;
+    {Sequence = []},
+    push_code(Code)
+    ).
+
+
+alpha_numeric_underline(Code) :- underline_code(Code).
+alpha_numeric_underline(Code) :- number_code(Code).
+alpha_numeric_underline(Code) :- lowercase_letter_code(Code).
+alpha_numeric_underline(Code) :- uppercase_letter_code(Code).
+
+backslash_code(Code) :- "\\" = [Code].
+quote_code(Code) :- "'" = [Code].
+underline_code(Code) :- "_" = [Code].
+number_code(Code) :- member(Code, "0123456789").
+lowercase_letter_code(Code) :- member(Code, "abcdefghijklmnopqrstuvwxyz").
+uppercase_letter_code(Code) :- member(Code, "ABCDEFGHIJKLMNOPQRSTUVWXYZ").
+
+
+push_code(Code, Full, [Code|Full]).
